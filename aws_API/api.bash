fc2c5dbac75f64b2e22b46de1e7debf9aa50f4b6
TOKB27 <152061606+TOKB27@users.noreply.github.com>
2026-04-15 12:28:01 +0900

    api.bash を更新

--- aws_API/api.bash
+++ aws_API/api.bash
@@ -4,12 +4,17 @@ USERNAME="<USERNAME>"
 PASSWORD="<PASSWORD>"
 API_URL="https://<api-id>.execute-api.ap-northeast-1.amazonaws.com/prod/sample"
 
-ID_TOKEN=$(aws cognito-idp initiate-auth \
-  --region "$REGION" \
-  --auth-flow USER_PASSWORD_AUTH \
-  --client-id "$CLIENT_ID" \
-  --auth-parameters USERNAME="$USERNAME",PASSWORD="$PASSWORD" \
-  | jq -r '.AuthenticationResult.IdToken')
+ID_TOKEN=$(curl -X POST https://cognito-idp.ap-northeast-1.amazonaws.com/ \
+  -H "Content-Type: application/x-amz-json-1.1" \
+  -H "X-Amz-Target: AWSCognitoIdentityProviderService.InitiateAuth" \
+  -d '{
+    "AuthFlow": "USER_PASSWORD_AUTH",
+    "ClientId": "xxx",
+    "AuthParameters": {
+      "USERNAME": "xxx",
+      "PASSWORD": "xxx"
+    }
+  }')
 
 curl "$API_URL" \
   -H "Authorization: Bearer $ID_TOKEN" \
